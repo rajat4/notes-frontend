bf77cc513a66725e93eac2e72e77c625c0e889d6
Rajat <rjtthwl@gmail.com>
2023-01-16 14:13:14 +0530

    blueshark

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./note-card.component.scss']
 })
 export class NoteCardComponent {
+
+  @Input('title')
+  title!: string;
+  @Input('body')
+  body!: string;
+
+  @ViewChild('truncator')
+  truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText')
+  bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() { 
+    let style = window.getComputedStyle(this.bodyText?.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText?.nativeElement.scrollHeight > viewableHeight) {
+
+      this.renderer.setStyle(this.truncator?.nativeElement, 'display', 'block');
+    } else {
+
+      this.renderer.setStyle(this.truncator?.nativeElement, 'display', 'none');
+
+    }
+  }
 
 }
